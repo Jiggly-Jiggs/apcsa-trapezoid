97c7033e8d2d12c4f2bb34508ddcf98218c0a3a6
Matthias Rago <mrago@ucvts.tec.nj.us>
2019-10-07 11:07:15 -0400

    finished

--- trapezoid.java
+++ trapezoid.java
@@ -5,9 +5,16 @@ public class trapezoid {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.print("\nInsert TOP value of a trapezoid : ");
         double top = in.nextDouble();
+        System.out.print("Insert BOTTOM value of a trapezoid : ");
         double bottom = in.nextDouble();
+        System.out.print("Insert HEIGHT value of a trapezoid : ");
         double height = in.nextDouble();
+
+        double area = (((top + bottom) / 2) * height);
+
+        System.out.printf("\nThe area of the trapezoid is %.1f", area);
         in.close();
       }
 }
