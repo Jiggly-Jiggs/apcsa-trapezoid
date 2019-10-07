dc232181d7392cedcc085356b40a15fb532c6679
Matthias Rago <mrago@ucvts.tec.nj.us>
2019-10-07 11:00:29 -0400

    initial commit

---
+++ trapezoid.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class trapezoid {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double top = in.nextDouble();
+        double bottom = in.nextDouble();
+        double height = in.nextDouble();
+        in.close();
+      }
+}
